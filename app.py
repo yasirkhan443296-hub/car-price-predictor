530c974fd679f1b277eebac8247a15a60550e5be
Farhan Khan <yasirkhan443296@gmail.com>
2026-06-01 12:29:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+pipe = pickle.load(open("car_price_model.pkl", "rb"))
+
+st.title("Car Price Predictor 🚗")
+
+name = st.text_input("Car Name")
+company = st.text_input("Company")
+year = st.number_input("Year", min_value=1990, max_value=2024)
+kms = st.number_input("KMs Driven")
+fuel = st.selectbox("Fuel Type", ["Petrol", "Diesel", "LPG"])
+
+if st.button("Predict Price"):
+    car = pd.DataFrame(
+            [[name, company, int(year), int(kms), fuel]],
+                    columns=["name", "company", "year", "kms_driven", "fuel_type"]
+                        )
+price = pipe.predict(car)[0]
+st.success(f"Estimated Price: ₹{round(price)}")
